df298748d8b9678a403bbe9bdae3b4f7b2cd9396
danzarov <danzarov@gmail.com>
2019-07-06 11:35:18 -0300

    not debugging anymoar

--- subprocess_manager.py
+++ subprocess_manager.py
@@ -1,8 +1,5 @@
 import subprocess
 
 def subprocess_call(command, item):
-    print('DEBUGGING -> ', command + item)
-    try:
-        subprocess.check_output(command + item, stderr=subprocess.STDOUT, shell=True)
-    except Exception as e:
-        raise e
+    subprocess.check_output(command + item, stderr=subprocess.STDOUT, shell=True)
+
